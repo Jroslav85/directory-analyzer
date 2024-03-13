518e71e8d27ce2dd8d2fff47fde810b8123d5547
Jroslav Trofimov <Jroslav1985@gmail.com>
2024-03-13 18:13:06 +0500

    added processing of incorrect path input

--- src/com/jroslav/main/StartApp.java
+++ src/com/jroslav/main/StartApp.java
@@ -1,4 +1,5 @@
 package com.jroslav.main;
+// 13.03.2024 directory analyzer
 
 import java.io.File;
 import java.util.Scanner;
@@ -7,19 +8,29 @@ import com.jroslav.main.controller.Controller;
 import com.jroslav.main.model.Analizer;
 import com.jroslav.main.view.View;
 
-// 13.03.2024 directory analyzer
-
 public class StartApp {
 
 	public static void main(String[] args) {
-		
-		try(Scanner console = new Scanner(System.in)) {
+
+		try (Scanner console = new Scanner(System.in)) {
 			System.out.println("Enter path");
-			String path = console.nextLine();
-			View view = new View(path);
-			Analizer analizer = new Analizer(new File(path));
-			Controller controller = new Controller(analizer, view);
-			controller.dataDispley();
+			analazePath(console);
 		}
+	}
+
+	private static void analazePath(Scanner console) {
+		do {
+			try {
+				final String path = console.nextLine();
+				final File file = new File(path);
+				final View view = new View(file.getName());
+				final Analizer analizer = new Analizer(file);
+				final Controller controller = new Controller(analizer, view);
+				controller.dataDispley();
+				break;
+			} catch (final Exception e) {
+				System.out.println("enter the correct path");
+			}
+		} while (true);
 	}
 }
